d5712d7ff1854068958b8cff83a70d53c0ca80b6
Steven Rodriguez <sgrodriguez@hotmail.com>
2015-05-03 21:18:25 -0700

    Basic file serializer done

--- quadcoptermedia/serializers.py
+++ quadcoptermedia/serializers.py
@@ -1,7 +1,9 @@
-from .models import UploadGroup, File
+from quadcoptermedia.models import UploadGroup, File
+from django.forms import widgets
 from rest_framework import serializers
 
 class FileSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = File
-		fields = ('date','uploaded_data')
+		fields = ('date', 'uploaded_file')
+
